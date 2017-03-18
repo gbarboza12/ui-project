56aee218133b22b9562ed857cc950e320a72f302
Guadalupe <gbarboza1231@gmail.com>
2017-03-18 17:01:08 -0500

    deleted files that were causing bug

--- app/overrides/sorting_links_in_products.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_products",
-                     :insert_before => "div#products",
-                     :partial       => "spree/shared/sorting_links",
-                     :name          => "sorting_links_in_products")
