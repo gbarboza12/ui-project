5cfd8dfc9f6d72e84c535c4a41e0e54f32f7e954
Guadalupe <gbarboza1231@gmail.com>
2017-03-18 01:31:18 -0500

    modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  protect_from_forgery 
+  include ApplicationHelper
+  helper_method :safe_params
   protected
     # Use this in place of params when generating links to Excel etc.
     # See https://github.com/rails/rails/issues/26289
     def safe_params
       params.except(:host, :port, :protocol).permit!
     end
-    helper_method :safe_params
+
 
 end
