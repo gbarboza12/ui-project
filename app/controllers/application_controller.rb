f7ec6bbf3a8eeff9a4fa26c26dd43f69bc88c264
Guadalupe <gbarboza1231@gmail.com>
2017-03-19 01:47:44 -0500

    login modal attempt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :safe_params
   helper_method :sorting_param
   protected
-    # Use this in place of params when generating links to Excel etc.
-    # See https://github.com/rails/rails/issues/26289
     def safe_params
       params.except(:host, :port, :protocol).permit!
     end
