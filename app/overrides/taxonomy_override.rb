bbdd75f8bd907ec8487558e68d1af783e80f6a3f
Guadalupe <gbarboza1231@gmail.com>
2017-03-20 16:07:57 -0500

    modified taxonomy override

--- app/overrides/taxonomy_override.rb
+++ app/overrides/taxonomy_override.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/shared/_taxonomies",
             :insert_before => "h4.taxonomy-root",
             :name          => "taxonomy_hide_category") do
-                "<% if taxonomy.name == 'Computers & Tablets' then next end %>"
+                "<% if taxonomy.name != 'Brands' then next end %>"
               end
